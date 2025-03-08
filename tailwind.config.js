450b133ac50704c0f837a41e6f5b85572750c16f
bschm0622 <rfcooper95@gmail.com>
2025-03-08 10:56:13 -0500

    styling and such

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,8 @@ module.exports = {
         "input-foreground": "var(--input-foreground)",
         destructive: "var(--destructive)",
         "destructive-foreground": "var(--destructive-foreground)",
+        link: "var(--link)",
+        "link-hover": "var(--link-hover)",
       },
       borderColor: {
         DEFAULT: "var(--border)",
